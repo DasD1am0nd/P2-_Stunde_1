a8330ae97509d867f2052be2186761368263d98d
DasD1am0nd <olivia.mock.spam@gmail.com>
2024-04-11 14:20:55 +0200

    update erste paus

---
+++ P2Ü_Stunde_1/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2Ü_Stunde_1
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber {  get; set; }
+
+        public Contact(string pFirstName, string pLastName)
+        {
+            FirstName = pFirstName;
+            LastName = pLastName;
+        }
+    }
+}
--- P2Ü_Stunde_1/Program.cs
+++ P2Ü_Stunde_1/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 1;
+            int b = 2;
+            int sum = a+b;
+            Console.WriteLine(sum);
+
+            var contact1 = new Contact("Nico","Stinkt");
+        }
+
+        static bool isEven(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
         }
     }
 }
