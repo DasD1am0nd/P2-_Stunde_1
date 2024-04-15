cfd586f3a4e32b8acc6fe3231357da8bec4f0de9
Olivia Mock <olivia.mock.spam@gmail.com>
2024-04-15 22:40:00 +0200

    update 12

---
+++ Temperatur/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Temperatur
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben Sie eine Temperatur und eine Einheit (C/F) ein.");
+            string? input = Console.ReadLine();
+            char[] inputChars = input.ToCharArray();
+            
+            string numberOut = "";
+            foreach (char c in inputChars)
+            {
+                switch(c)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': numberOut+= c; break;
+                    default: break;
+                }
+            }
+
+            if (input.Contains('f') || input.Contains('F'))
+            {
+                Console.WriteLine($"{input} entspechen {((Convert.ToDouble(numberOut)-32)*5/9)}°C");
+            }
+            if (input.Contains('c') || input.Contains('C'))
+            {
+                Console.WriteLine($"{input} entspechen {((Convert.ToDouble(numberOut) * 1.8) + 32)}°F");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vokale_zaehlen/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Vokale_zaehlen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countVowel("Hello World"));
+        }
+        static int countVowel(string pText)
+        {
+            int chars=0;
+            foreach (char c in pText)
+            {
+                chars++;
+            }
+            return chars;
+        }
+    }
+}
